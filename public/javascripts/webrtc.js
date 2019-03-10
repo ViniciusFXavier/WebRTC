da3910158cea28e363331b7e3b72f914788ff2f0
Vinícius Francisco Xavier <viniciusfranciscoxavier@gmail.com>
2019-03-10 17:38:34 -0300

    Ajustes

--- public/javascripts/webrtc.js
+++ public/javascripts/webrtc.js
@@ -1,295 +1,174 @@
-var conf = { iceServers: [{ urls: [] }] };
-var pc = new RTCPeerConnection(conf);
-var localStream, _fileChannel, chatEnabled, context, source,
-	_chatChannel, sendFileDom = {},
-	recFileDom = {}, receiveBuffer = [],
-	receivedSize = 0,
-	file,
-	bytesPrev = 0;
+var configuration = { iceServers: [{ urls: [] }] };
+const localConnection = new RTCPeerConnection(configuration);
+var remoteConnection = null;
+var localStream = new MediaStream();
+
+var channel = [];
+
 function errHandler(err) {
-	console.log(err);
-}
-function enableChat() {
-	enable_chat.checked ? (chatEnabled = true) : (chatEnabled = false);
-}
-enableChat();
-
-navigator.mediaDevices.getUserMedia({ audio: true, video: true }).then(stream => {
-	localStream = stream;
-	micused.innerHTML = localStream.getAudioTracks()[0].label;
-	pc.addStream(stream);
-	local.src = URL.createObjectURL(stream);
-	local.muted = true;
-}).catch(errHandler);
-
-function sendMsg() {
-	var text = sendTxt.value;
-	chat.innerHTML = chat.innerHTML + "<pre class=sent>" + text + "</pre>";
-	_chatChannel.send(text);
-	sendTxt.value = "";
-	return false;
+	console.warn(err);
 }
 
-pc.ondatachannel = function (e) {
-	if (e.channel.label == "fileChannel") {
-		console.log('fileChannel Received -', e);
-		_fileChannel = e.channel;
-		fileChannel(e.channel);
-	}
-	if (e.channel.label == "chatChannel") {
-		console.log('chatChannel Received -', e);
-		_chatChannel = e.channel;
-		chatChannel(e.channel);
-	}
-};
-
-pc.onicecandidate = function (e) {
-	var cand = e.candidate;
-	if (!cand) {
-		console.log('iceGatheringState complete', pc.localDescription.sdp);
-		localOffer.value = JSON.stringify(pc.localDescription);
-	} else {
-		console.log(cand.candidate);
+function init(err) {
+	if (navigator.mediaDevices.getUserMedia === undefined) {
+		enableCameraStream.disabled = true;
 	}
 }
-pc.oniceconnectionstatechange = function () {
-	console.log('iceconnectionstatechange: ', pc.iceConnectionState);
-}
-pc.onaddstream = function (e) {
-	console.log('remote onaddstream', e.stream);
-	remote.src = URL.createObjectURL(e.stream);
-}
-pc.onconnectionstatechange = function (event) {
-	console.log('onconnectionstatechange ', event);
-}
+init();
 
+//Connection
 remoteOfferGot.onclick = function () {
-	var _remoteOffer = new RTCSessionDescription(JSON.parse(remoteOffer.value));
-	console.log('remoteOffer \n', _remoteOffer);
-	pc.setRemoteDescription(_remoteOffer).then(function () {
+	remoteConnection = new RTCSessionDescription(JSON.parse(remoteOffer.value));
+	console.log('remoteOffer \n', remoteConnection);
+	localConnection.setRemoteDescription(remoteConnection).then(function () {
 		console.log('setRemoteDescription ok');
-		if (_remoteOffer.type == "offer") {
-			pc.createAnswer().then(function (description) {
+		if (remoteConnection.type == "offer") {
+			localConnection.createAnswer().then(function (description) {
 				console.log('createAnswer 200 ok \n', description);
-				pc.setLocalDescription(description).then(function () {
+				localConnection.setLocalDescription(description).then(function () {
 				}).catch(errHandler);
 			}).catch(errHandler);
 		}
 	}).catch(errHandler);
 }
 localOfferSet.onclick = function () {
-	if (chatEnabled) {
-		_chatChannel = pc.createDataChannel('chatChannel');
-		_fileChannel = pc.createDataChannel('fileChannel');
-		// _fileChannel.binaryType = 'arraybuffer';
-		chatChannel(_chatChannel);
-		fileChannel(_fileChannel);
+	if (enableChat.checked) {
+		channel.chat = localConnection.createDataChannel('chat');
+		chatChannel(channel.chat);
 	}
-	pc.createOffer({iceRestart: true}).then(des => {
+	localConnection.createOffer().then(des => {
 		console.log('createOffer ok ');
-		pc.setLocalDescription(des).then(() => {
+		localConnection.setLocalDescription(des).then(() => {
 			setTimeout(function () {
-				if (pc.iceGatheringState == "complete") {
+				if (localConnection.iceGatheringState == "complete") {
 					return;
 				} else {
 					console.log('after GetherTimeout');
-					localOffer.value = JSON.stringify(pc.localDescription);
+					localOffer.value = JSON.stringify(localConnection.localDescription);
 				}
 			}, 2000);
 			console.log('setLocalDescription ok');
 		}).catch(errHandler);
-		// For chat
 	}).catch(errHandler);
 }
 
-//File transfer
-fileTransfer.onchange = function (e) {
-	var files = fileTransfer.files;
-	if (files.length > 0) {
-		file = files[0];
-		sendFileDom.name = file.name;
-		sendFileDom.size = file.size;
-		sendFileDom.type = file.type;
-		sendFileDom.fileInfo = "areYouReady";
-		console.log(sendFileDom);
-	} else {
-		console.log('No file selected');
+//Channel
+localConnection.ondatachannel = function (event) {
+	if (event.channel.label == "chat") {
+		console.log('chat Channel Received -', event);
+		channel.chat = event.channel;
+		chatChannel(event.channel);
 	}
-}
-function sendFile() {
-	if (!fileTransfer.value) return;
-	var fileInfo = JSON.stringify(sendFileDom);
-	_fileChannel.send(fileInfo);
-	console.log('file info sent');
-}
-
-
-function fileChannel(e) {
-	_fileChannel.onopen = function (e) {
-		console.log('file channel is open', e);
+};
+function chatChannel(event) {
+	channel.chat.onopen = function (event) {
+		console.log('chat channel is open', event);
 	}
-	_fileChannel.onmessage = function (e) {
-		// Figure out data type
-		var type = Object.prototype.toString.call(e.data), data;
-		if (type == "[object ArrayBuffer]") {
-			data = e.data;
-			receiveBuffer.push(data);
-			receivedSize += data.byteLength;
-			recFileProg.value = receivedSize;
-			if (receivedSize == recFileDom.size) {
-				var received = new window.Blob(receiveBuffer);
-				file_download.href = URL.createObjectURL(received);
-				file_download.innerHTML = "download";
-				file_download.download = recFileDom.name;
-				// rest
-				receiveBuffer = [];
-				receivedSize = 0;
-				// clearInterval(window.timer);	
-			}
-		} else if (type == "[object String]") {
-			data = JSON.parse(e.data);
-		} else if (type == "[object Blob]") {
-			data = e.data;
-			file_download.href = URL.createObjectURL(data);
-			file_download.innerHTML = "download";
-			file_download.download = recFileDom.name;
-		}
-
-		// Handle initial msg exchange
-		if (data.fileInfo) {
-			if (data.fileInfo == "areYouReady") {
-				recFileDom = data;
-				recFileProg.max = data.size;
-				var sendData = JSON.stringify({ fileInfo: "readyToReceive" });
-				_fileChannel.send(sendData);
-				// window.timer = setInterval(function(){
-				// 	Stats();
-				// },1000)				
-			} else if (data.fileInfo == "readyToReceive") {
-				sendFileProg.max = sendFileDom.size;
-				sendFileinChannel(); // Start sending the file
-			}
-			console.log('_fileChannel: ', data.fileInfo);
-		}
+	channel.chat.onmessage = function (event) {
+		console.log('chat channel have message', event);
+		chat.innerHTML = chat.innerHTML + "<pre>" + event.data + "</pre>"
 	}
-	_fileChannel.onclose = function () {
-		console.log('file channel closed');
-	}
-}
-
-function chatChannel(e) {
-	_chatChannel.onopen = function (e) {
-		console.log('chat channel is open', e);
-	}
-	_chatChannel.onmessage = function (e) {
-		chat.innerHTML = chat.innerHTML + "<pre>" + e.data + "</pre>"
-	}
-	_chatChannel.onclose = function () {
+	channel.chat.onclose = function () {
 		console.log('chat channel closed');
 	}
 }
 
-function sendFileinChannel() {
-	var chunkSize = 16384;
-	var sliceFile = function (offset) {
-		var reader = new window.FileReader();
-		reader.onload = (function () {
-			return function (e) {
-				_fileChannel.send(e.target.result);
-				if (file.size > offset + e.target.result.byteLength) {
-					window.setTimeout(sliceFile, 0, offset + chunkSize);
-				}
-				sendFileProg.value = offset + e.target.result.byteLength
-			};
-		})(file);
-		var slice = file.slice(offset, offset + chunkSize);
-		reader.readAsArrayBuffer(slice);
-	};
-	sliceFile(0);
-}
-
-function Stats() {
-	pc.getStats(null, function (stats) {
-		for (var key in stats) {
-			var res = stats[key];
-			console.log(res.type, res.googActiveConnection);
-			if (res.type === 'googCandidatePair' &&
-				res.googActiveConnection === 'true') {
-				// calculate current bitrate
-				var bytesNow = res.bytesReceived;
-				console.log('bit rate', (bytesNow - bytesPrev));
-				bytesPrev = bytesNow;
-			}
-		}
+// Get Media
+enableScreenStream.onclick = function () {
+	stopMedia();
+	navigator.mediaDevices.getDisplayMedia({ video: SVGComponentTransferFunctionElement }).then(function (stream) {
+		console.log('screenStream', stream);
+		localVideoStream.srcObject = stream;
+		localConnection.addTrack(stream.getTracks()[0], stream);
+	}).catch(function (error) {
+		console.log('Screen: ' + error.name, error.message);
 	});
 }
+enableCameraStream.onclick = function () {
+	stopMedia();
+	navigator.mediaDevices.getUserMedia({ audio: enableMicrophoneStream.checked, video: true }).then(function (stream) {
+		console.log('mediastream', stream);
+		localVideoStream.srcObject = stream;
+		stream.getTracks().forEach(function(track) {
+			localConnection.addTrack(track, stream);
+		});
+	}).catch(function (error) {
+		console.log('Media: ' + error.name, error.message);
+	});
+}
+function stopMedia(){
+	var stream = localVideoStream.srcObject;
+	if (stream){
+		stream.getTracks().forEach(function (track) {
+			track.stop();
+		});
+	}
+	localVideoStream.srcObject = null;
+}
 
-streamAudioFile.onchange = function () {
-	console.log('streamAudioFile');
-	context = new AudioContext();
-	var file = streamAudioFile.files[0];
-	if (file) {
-		if (file.type.match('audio*')) {
-			var reader = new FileReader();
-			reader.onload = (function (readEvent) {
-				context.decodeAudioData(readEvent.target.result, function (buffer) {
-					// create an audio source and connect it to the file buffer
-					source = context.createBufferSource();
-					source.buffer = buffer;
-					source.start(0);
+localConnection.onicecandidate = function (event) {
+	var cand = event.candidate;
+	console.log('cand', cand);
+	if (!cand) {
+		console.log('iceGatheringState complete', localConnection.localDescription);
+		localOffer.value = JSON.stringify(localConnection.localDescription);
+	} else {
+		console.log('candidate', cand);
+	}
+}
+localConnection.ontrack = function (event) {
+	remoteVideoStream.srcObject = event.streams[0];
+	remoteVideoStream.play();
+};
 
-					// connect the audio stream to the audio hardware
-					source.connect(context.destination);
+localConnection.oniceconnectionstatechange = function () {
+	console.log('iceconnectionstatechange: ', localConnection.iceConnectionState);
+	connectionStatus.innerHTML = localConnection.iceConnectionState;
+}
 
-					// create a destination for the remote browser
-					var remote = context.createMediaStreamDestination();
+localConnection.onconnectionstatechange = function (event) {
+	console.log('onconnectionstatechange ', localConnection.connectionState);
+	connectionStatus.innerHTML = localConnection.connectionState;
+	if (localConnection.connectionState === "connected") {
 
-					// connect the remote destination to the source
-					source.connect(remote);
+	} else if (localConnection.connectionState === "disconnected") {
 
-					local.srcObject = remote.stream
-					local.muted = true;
-					// add the stream to the peer connection
-					pc.addStream(remote.stream);
+	} else if (localConnection.connectionState === "failed") {
 
-					// create a SDP offer for the new stream
-					// pc.createOffer(setLocalAndSendMessage);
-				});
-			});
+	} else if (localConnection.connectionState === "closed") {
 
-			reader.readAsArrayBuffer(file);
-		}
 	}
 }
 
-var audioRTC = function (cb) {
-	console.log('streamAudioFile');
-	window.context = new AudioContext();
-	var file = streamAudioFile.files[0];
-	if (file) {
-		if (file.type.match('audio*')) {
-			var reader = new FileReader();
-			reader.onload = (function (readEvent) {
-				context.decodeAudioData(readEvent.target.result, function (buffer) {
-					// create an audio source and connect it to the file buffer
-					var source = context.createBufferSource();
-					source.buffer = buffer;
-					source.start(0);
+function sendMsg() {
+	var text = sendTxt.value;
+	chat.innerHTML = chat.innerHTML + "<pre class='sent'>" + text + "</pre>";
+	channel.chat.send(text);
+	sendTxt.value = "";
+	return false;
+}
 
-					// connect the audio stream to the audio hardware
-					source.connect(context.destination);
-
-					// create a destination for the remote browser
-					var remote = context.createMediaStreamDestination();
-
-					// connect the remote destination to the source
-					source.connect(remote);
-					window.localStream = remote.stream;
-					cb({ 'status': 'success', 'stream': true });
-				});
+// View
+enableChat.addEventListener('change', function () {
+	var divChat = document.getElementById("divChat");
+	if (this.checked) {
+		channel.chat = localConnection.createDataChannel('chat');
+		chatChannel(channel.chat);
+	} else {
+		channel.chat.close();
+	}
+});
+enableMicrophoneStream.addEventListener('change', function () {
+	var stream = localVideoStream.srcObject;
+	if (stream){
+		if (this.checked) {
+			stream.getAudioTracks().forEach(function (track) {
+				track.enable = true;
 			});
-
-			reader.readAsArrayBuffer(file);
+		} else {
+			stream.getAudioTracks().forEach(function (track) {
+				track.enable = false;
+			});
 		}
 	}
-}
+});
